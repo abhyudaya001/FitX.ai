6e092d4ff1d0efcc93b4135ec1d97a9847634a5a
abhyudayakandwal <abhyudayakandwal001@gmail.com>
2025-12-24 08:47:03 +0530

    Configure Kafka for Asynchronous communication between Activity service and AI service Day6:)

--- activityservice/src/main/java/com/example/activityservice/services/ActivityService.java
+++ activityservice/src/main/java/com/example/activityservice/services/ActivityService.java
@@ -6,6 +6,8 @@ import com.example.activityservice.dto.ActivityResponse;
 import com.example.activityservice.model.Activity;
 import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,6 +15,10 @@ import org.springframework.stereotype.Service;
 public class ActivityService {
     private final ActivityRepository activityRepository;
     private final UserValidationService userValidationService;
+    private final KafkaTemplate<String,Activity> kafkaTemplate;
+
+    @Value("${kafka.topic.name}")
+    private String topicName;
     public ActivityResponse trackActivity(ActivityRequest request) {
         boolean isValidUser = userValidationService.validateUser(request.getUserId());
         if(!isValidUser){
@@ -20,6 +26,11 @@ public class ActivityService {
         }
         Activity activity = Activity.builder().userId(request.getUserId()).type(request.getType()).duration(request.getDuration()).caloriesBurned(request.getCaloriesBurned()).startTime(request.getStartTime()).additionalMetrics(request.getAdditionalMetrics()).build();
         Activity savedActivity = activityRepository.save(activity);
+        try {
+            kafkaTemplate.send(topicName,savedActivity.getUserId(),savedActivity);
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
         return mapToResponse(savedActivity);
     }
 
